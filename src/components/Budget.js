c652cafb4937979968e899b0e58ae71d96fef4dc
SudaraWishvajith <it21221378@my.sliit.lk>
2023-06-27 13:35:18 +0530

    Implement component Budget
    In this will be importing app context and the useContext hook, and pass your AppContext to it - this is how a component connects to the context in order to get values from global state.

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,12 @@
+import React, { useContexet } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = ()=>{
+    const { budget } = useContexet(AppContext);
+    return(
+        <div className='alert alert-secondary'>
+            <span>Budget: ${budget}</span>
+        </div>
+    )
+}
 
+export default Budget;
