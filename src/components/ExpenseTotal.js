668edf893fe6c626c8531fc6c93360c5dd8b54ce
SudaraWishvajith <it21221378@my.sliit.lk>
2023-06-27 14:48:36 +0530

    implement ExpenseTotal component
     get expenses using useContext and AppContext and calculate total Expenses

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,17 @@
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const ExpenseTotal = ()=>{
+    const { expenses, budget } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((tExpense,item)=>{
+        return (tExpense = tExpense + item.cost);
+    },0)
+
+    return(
+        <div className='alert alert-primary'>
+            <span>Spent so far: ${totalExpenses}</span>
+        </div>
+    );
+};
+
+export default ExpenseTotal;
